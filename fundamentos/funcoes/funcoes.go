523c665ebcc2df5924085d24b5b6ff5767929c03
Pedro <p-cardozo@hotmail.com>
2023-02-08 07:44:24 -0300

    feat(functions): incluing basic functions

---
+++ fundamentos/funcoes/funcoes.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func skibi(a int, b int) int {
+	return a + b
+}
+
+func show(a int) {
+	fmt.Println("Input: ", a)
+}
---
+++ fundamentos/funcoes/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	show(skibi(1, 2))
+}
